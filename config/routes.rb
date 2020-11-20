8eb9b29ad4beb564f770c1b03a582f254d395f46
Wataru Ono <wtr0414@gmail.com>
2020-11-20 11:53:49 +0900

    :sparkles: コントローラの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get     'tasks'     => 'tasks#index'
+  post    'tasks'     => 'tasks#store'
+  get     'tasks/:id' => 'tasks#show'
+  put     'tasks/:id' => 'tasks#update'
+  delete  'tasks/:id' => 'tasks#destroy'
 end
